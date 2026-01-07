0360ecb161fe51ef45e8292af89be618277e01e2
Atomjoy <atomjoy.official@gmail.com>
2026-01-07 10:44:39 +0100

    tsconfig vue laravel

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,7 +1,13 @@
-// import axios from 'axios';
+import axios, { AxiosStatic } from 'axios';
+
+declare global {
+	interface Window {
+		axios: AxiosStatic;
+	}
+}
 
 // axios.defaults.withCredentials = true;
 // axios.defaults.withXSRFToken = true;
 
-// window.axios = axios;
-// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios = axios;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
