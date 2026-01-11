82d748bc1416c15c5a89f3712411bb850ecf96c2
Atomjoy <atomjoy.official@gmail.com>
2026-01-11 16:38:45 +0100

    Theme colors xkom

--- resources/js/components/example/Theme.vue
+++ resources/js/components/example/Theme.vue
@@ -8,7 +8,16 @@ import ChangeTheme from '../utils/head/ChangeTheme.vue';
 
 		<div class="card">
 			<h1>Login</h1>
-			<div class="card-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum iusto natus ad dignissimos, aliquam, rerum maiores voluptatibus ipsam pariatur velit perferendis soluta molestias earum eveniet nihil magnam iure explicabo.</div>
+			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur impedit amet quos expedita aspernatur? Cumque veritatis eveniet distinctio in animi sapiente, autem eos debitis quaerat natus molestiae ad expedita quos.</p>
+
+			<div class="card-info">
+				<h3>Info window</h3>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum iusto natus ad dignissimos, aliquam, rerum maiores voluptatibus ipsam pariatur velit perferendis soluta molestias earum eveniet nihil magnam iure explicabo.
+			</div>
+
+			<div class="card-error">Invalid password.</div>
+
+			<div class="card-success">Account has been created.</div>
 
 			<label for="email">Email</label>
 			<input type="text" name="email" />
@@ -31,6 +40,7 @@ input {
 	margin-block: 10px;
 	border: 1px solid var(--border);
 	border-radius: var(--border-radius);
+	background: var(--field);
 }
 
 button {
@@ -53,12 +63,13 @@ button:hover {
 
 label {
 	color: var(--label);
+	font-weight: 500;
 }
 
 .bg {
 	float: left;
 	width: 100%;
-	height: 100vh;
+	min-height: 100vh;
 	padding: 40px;
 	background: var(--bg-1);
 	color: var(--text-1);
@@ -79,8 +90,28 @@ label {
 	width: 100%;
 	padding: 20px;
 	margin-block: 20px;
+	border-radius: var(--border-radius);
 	background: var(--bg-3);
 	color: var(--text-3);
+}
+
+.card-error {
+	float: left;
+	width: 100%;
+	padding: 20px;
+	margin-block: 20px;
 	border-radius: var(--border-radius);
+	background: var(--alert-error-bg);
+	color: var(--alert-error);
+}
+
+.card-success {
+	float: left;
+	width: 100%;
+	padding: 20px;
+	margin-block: 20px;
+	border-radius: var(--border-radius);
+	background: var(--alert-success-bg);
+	color: var(--alert-success);
 }
 </style>
